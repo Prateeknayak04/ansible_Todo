2f1e7cbca02be1fade3ce68c99c5bcfc382a18f2
Prateeknayak04 <nprateek213@gmail.com>
2025-08-05 07:26:11 +0000

    56.0th commit

--- website/__init__.py
+++ website/__init__.py
@@ -10,7 +10,7 @@ DB_NAME= "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "abcdefghijkl" #os.getenv("secret")
+    app.config["SECRET_KEY"] = os.getenv("secret") #"abcdefghijkl"
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
     db.init_app(app)
 
